cfc5c124079519557cca75fd49ece55957c41ae8
songdonghong <songdonghong@daojia-inc.com>
2019-10-08 17:39:54 +0800

    增加table可滚动

--- dist/table/index.js
+++ dist/table/index.js
@@ -1,4 +1,5 @@
 // components/table/index.js
+
 Component({
   options: {
     styleIsolation: "isolated"
@@ -18,15 +19,60 @@ Component({
     hasBorder: {
       type: String,
       value: "no"
+    },
+    height: { //table的高度
+      type: Number || String,
+      value: ''
+    },
+    width: {
+      type: Number,
+      value: 0
+    },
+    tdWidth: {
+      type: Number,
+      value: 35
     }
 
   },
   externalClasses: ['s-class-header', 's-class-row'],
+  lifetimes: {
+    attached: function() {
+      this.createSelectorQuery().select('.thead > .td').boundingClientRect( (rect) => {
+        console.log('rect', rect)
+        let tempWidth = rect.width * this.properties.headers.length 
+        if (this.properties.width > 0 && this.properties.width < tempWidth){
+          this.setData({
+            theadWidth: tempWidth,
+            scrolWidth: this.properties.width
+          })          
+        }else{
+          this.setData({
+            theadWidth: tempWidth,
+            scrolWidth: tempWidth
+          })
+        } 
+        let tempHeight = rect.height * this.properties.list.length 
+        if (tempHeight < this.properties.height){
+          this.setData({
+            height: this.properties.height
+          })
+
+        }
+      }).exec()
+      // 在组件实例进入页面节点树时执行
+      
+    },
+    detached: function() {
+      // 在组件实例被从页面节点树移除时执行
+    }
+  },
 
   /**
    * 组件的初始数据
    */
   data: {
+    theadWidth: 0,    
+    scrolWidth:0
   },
 
   /**
--- index/index.js
+++ index/index.js
@@ -2,14 +2,18 @@ const app = getApp()
 
 Page({
   data: {
-    headers: [{ text: 'one', display: '第一列' }, { text: 'two', display: '第二列' }, { text: 'three', display: '第三列' }, { text: 'four', display: '第四列' }],
+    headers: [{ text: 'one', display: '第一列' }, { text: 'two', display: '第二列' }, { text: 'three', display: '第三列' }, { text: 'four', display: '第四列' }, { text: 'five', display: '第五列' }],
     row:[]
 
   },
   onLoad: function () {
     let tempData = []
     for(let i=1;i<10;i++){
-      tempData.push({ one: "第" + i + "行", two: "第" + i + "行", three: "第" + i + "行", four: "第" + i + "行"})            
+      let tempObj={}
+      for(let j=0;j<this.data.headers.length;j++){
+        tempObj[this.data.headers[j]['text']] = "第" + i+"行"
+      }
+      tempData.push(tempObj)        
     }
     this.setData({
       row:tempData
